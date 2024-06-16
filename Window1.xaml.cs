977501b638670a6990b867b7a3fa64dfa5080f99
Rezbirn <rezbirn@gmail.com>
2024-06-16 22:43:39 +0300

    Add project files.

---
+++ Window1.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UI_2
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        int count;
+        public Window1()
+        {
+            InitializeComponent();
+        }
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = true;
+            if (MessageBox.Show("Закрити підпорядковане вікно?", "Підтвердження",
+            MessageBoxButton.YesNo,
+            MessageBoxImage.Question, MessageBoxResult.No) == MessageBoxResult.Yes)
+            {
+                Hide();
+                Owner.Activate();
+            }
+
+        }
+        private void Window_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            (Owner.FindName("button1") as Button).Content = IsVisible ?
+            "Закрити підпорядковане вікно" : "Відкрити підпорядковане вікно";
+            if (IsVisible)
+                textBlock.Text = "Вікно відкрите " + (++count) + "-й раз.";
+        }
+
+    }
+}
---
+++ Window2.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UI_2
+{
+    /// <summary>
+    /// Interaction logic for Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        bool dialogRes;
+        public bool DialogRes
+        {
+            get { return dialogRes; }
+        }
+
+        public Window2()
+        {
+            InitializeComponent();
+        }
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = true;
+            Hide();
+        }
+        private void Window_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (IsVisible) 
+                dialogRes = false;
+        }
+        private void button21_Click(object sender, RoutedEventArgs e)
+        {
+            dialogRes = true;
+            Close();
+        }
+        public void button22_Click(object sender, RoutedEventArgs e)
+        {
+            Owner.Title = textBox1.Text;
+            Owner.OwnedWindows[0].Title = textBox2.Text;
+        }
+    }
+}
